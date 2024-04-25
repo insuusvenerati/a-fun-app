7d8299185bb03115462eeb13adfa4b4da0c8753d
Sean Norwood <norwood.sean@gmail.com>
2024-04-24 20:53:48 -0500

    Add sidebar, nav, and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import logo from "@/assets/react.svg";
 
 function App() {
 	return (
-		<header className="flex items-center gap-4 border-b p-4 shadow-sm">
-			<img
-				src={logo}
-				alt="React Logo"
-				className="h-12 w-12 hover:animate-spin"
-			/>
-			<h1 className="text-2xl font-bold hover:underline">
-				<a href="/">A Fun App</a>
-			</h1>
-			<nav></nav>
-		</header>
+		<div className="flex flex-col gap-4">
+			<header className="flex items-center gap-4 rounded-md border-b bg-slate-50 p-4 shadow-md">
+				<img
+					src={logo}
+					alt="React Logo"
+					className="h-12 w-12 hover:animate-spin"
+				/>
+				<h1 className="text-2xl font-bold hover:underline">
+					<a href="/">A Fun App</a>
+				</h1>
+				<nav></nav>
+			</header>
+			<div className="flex border-r">
+				<div className="h-screen w-1/4 rounded-md bg-slate-50 p-4 shadow-md">
+					<h1>Sidebar</h1>
+				</div>
+			</div>
+		</div>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+		<div className="p-4">
+			<App />
+		</div>
+	</React.StrictMode>,
 );
